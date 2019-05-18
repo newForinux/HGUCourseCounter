07a9c452a05d942e4f061a7d01f90a05eb82a024
newForinux <21600065@handong.edu>
2019-05-18 15:12:20 +0900

    Add get methods and revise constructor

--- src/main/java/edu/handong/analysis/datamodel/Course.java
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -12,6 +12,51 @@ public class Course {
 	int semesterCourseTaken;
 	
 	public Course (String line) {
-		
+		String[] array_str = line.split(",");
+		studentId = array_str[0];
+		yearMonthGraduated = array_str[1];
+		firstMajor = array_str[2];
+		secondMajor = array_str[3];
+		courseCode = array_str[4];
+		courseName = array_str[5];
+		courseCredit = array_str[6];
+		yearTaken = Integer.parseInt(array_str[7]);
+		semesterCourseTaken = Integer.parseInt(array_str[8]);
+	}
+	
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public String getYearMonthGraduated() {
+		return yearMonthGraduated;
+	}
+
+	public String getFirstMajor() {
+		return firstMajor;
+	}
+
+	public String getSecondMajor() {
+		return secondMajor;
+	}
+
+	public String getCourseCode() {
+		return courseCode;
+	}
+
+	public String getCourseName() {
+		return courseName;
+	}
+
+	public String getCourseCredit() {
+		return courseCredit;
+	}
+
+	public int getYearTaken() {
+		return yearTaken;
+	}
+
+	public int getSemesterCourseTaken() {
+		return semesterCourseTaken;
 	}
 }
