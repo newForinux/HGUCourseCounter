141d214e848086149547248a69e49cf9a961acbc
newForinux <21600065@handong.edu>
2019-05-18 15:14:58 +0900

    Implemented Student class

--- src/main/java/edu/handong/analysis/datamodel/Student.java
+++ src/main/java/edu/handong/analysis/datamodel/Student.java
@@ -12,14 +12,37 @@ public class Student {
 	}
 	
 	public void addCourse (Course newRecord) {
-		
+		coursesTaken.add(newRecord);
 	}
 	
 	public HashMap<String, Integer> getSemestersByYearAndSemester() {
+		int semester = 0, temp = 0;
+		String key = null;
+		HashMap<String, Integer> getter = new HashMap<String, Integer>();
 		
+		for (int i = 0; i < coursesTaken.size(); i++) {
+			if (i == 0 || temp == coursesTaken.get(i).semesterCourseTaken) {
+				key = coursesTaken.get(i).yearTaken + "-" + coursesTaken.get(i).semesterCourseTaken;
+				semester++;
+				temp = semester;
+				getter.put(key, semester);
+			}
+		}
+		
+		return getter;
 	}
 	
-	public int getNumCourseInNthSemester (int semeseter) {
+	public int getNumCourseInNthSemester (int semester) {
+		HashMap<String, Integer> getter = getSemestersByYearAndSemester();
+		String key = null;
+		int countNum = 0;
 		
+		for (int i = 0; i < coursesTaken.size(); i++) {
+			key = coursesTaken.get(i).yearTaken + "-" + coursesTaken.get(i).semesterCourseTaken;
+			if (semester == getter.get(key))
+				countNum++;
+		}
+		
+		return countNum;
 	}
 }
