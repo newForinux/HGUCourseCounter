745f31a8eabc686c2f57ca0a33a41c79a1616e47
newForinux <21600065@handong.edu>
2019-05-18 15:13:11 +0900

    Revise removeHeader bool logic

--- src/main/java/edu/handong/analysis/utils/Utils.java
+++ src/main/java/edu/handong/analysis/utils/Utils.java
@@ -1,12 +1,45 @@
 package edu.handong.analysis.utils;
 
+import java.io.*;
+import java.util.*;
+
 public class Utils {
 
 	public static ArrayList<String> getLines (String file, boolean removeHeader) {
+		Scanner inputStream = new Scanner(file);
+		ArrayList<String> result = new ArrayList<String>();
+		String line = new String();
 		
+		while (inputStream.hasNextLine())
+		{
+			line = inputStream.nextLine();
+			result.add(line);
+		}
+		
+		if (removeHeader)
+			result.remove(0);
+		
+		inputStream.close();
+		return result;
 	}
 	
 	public static void writeAFile (ArrayList<String> lines, String targetFileName) {
 		
+		PrintWriter outputStream = null;
+		
+		try
+		{
+			outputStream = new PrintWriter(new FileOutputStream(targetFileName));
+			
+		}catch (FileNotFoundException e)
+		{
+			File mkdir_path = new File(targetFileName);
+			mkdir_path.getParentFile().mkdirs();
+		}
+		
+		for (String line:lines) {
+			outputStream.println(line);
+		}
+		outputStream.close();
 	}
 }
